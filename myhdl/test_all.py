45b4e25be40bb79cc2f187000d623117cd106787
jand <none@none>
2003-01-23 23:21:31 +0000

    Put into a project

--- myhdl/test_all.py
+++ myhdl/test_all.py
@@ -1,5 +1,4 @@
 import test_Simulation, test_Signal, test_intbv
-from demo.arith_lib import test_all
 
 modules = (test_Simulation, test_Signal, test_intbv)
 
@@ -10,7 +9,6 @@ def suite():
     alltests = unittest.TestSuite()
     for m in modules:
         alltests.addTest(tl.loadTestsFromModule(m))
-    alltests.addTest(test_all.suite())
     return alltests
 
 if __name__ == '__main__':
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="myhdl",
+      version="0.1",
+      description="myhdl python library",
+      author="Jan Decaluwe",
+      author_email="jan@jandecaluwe.com",
+      url="www.jandecaluwe.com",
+      packages=['myhdl']
+      )
+      
