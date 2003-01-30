1f90677d404f03959f9c91a75b7161b0e89e36f5
jand <none@none>
2003-01-30 23:04:36 +0000

    manual code started

---
+++ example/manual/HelloWorld.py
@@ -0,0 +1,20 @@
+from __future__ import generators
+from myhdl import Signal, delay, posedge, now, Simulation
+
+clk = Signal(0)
+
+def clkGen():
+    while 1:
+        yield delay(10)
+        clk.next = 1
+        yield delay(10)
+        clk.next = 0
+
+def sayHello():
+    while 1:
+        yield posedge(clk)
+        print "%s Hello World!" % now()
+
+sim = Simulation(clkGen(), sayHello())
+sim.run(100)
+
