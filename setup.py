b8d77aca7b9ccd1796ec8fe3990f4ce02a7e701b
jand <none@none>
2003-01-30 17:19:02 +0000

    version check

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 """ myhdl's distutils distribution and installation script. """
 
-from distutils.core import setup
+import sys
 
+versionError = "ERROR: myhdl requires Python 2.2.2 or higher"
+
+# use version_info to check version
+# this was new in 2.0, so first see if it exists
+try:
+    sys.version_info
+except:
+    print versionError
+    raise SystemExit(1)
+# we need at least 2.2.2
+if sys.version_info[:3] < (2, 2, 2):
+    print versionError
+    raise SystemExit(1)
+
+from distutils.core import setup
+    
 setup(name="myhdl",
       version="0.1",
       description="myhdl python library",
