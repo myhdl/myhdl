9d955c6f10c821296532afd704d2a83d063c3e02
jand <none@none>
2003-04-24 14:35:30 +0000

    avoid lingering signals for performance

--- example/manual/test_gray.py
+++ example/manual/test_gray.py
@@ -7,7 +7,7 @@ from myhdl import Simulation, Signal, delay, intbv, bin
 
 from bin2gray import bin2gray
 
-MAX_WIDTH = 10
+MAX_WIDTH = 11
 
 def nextLn(Ln):
     """ Return Gray code Ln+1, given Ln. """
@@ -27,11 +27,9 @@ class TestOriginalGrayCode(TestCase):
         
         """ Check that the code is an original Gray code """
 
-        B = Signal(intbv(-1))
-        G = Signal(intbv(0))
         Rn = []
         
-        def stimulus(n):
+        def stimulus(B, G, n):
             for i in range(2**n):
                 B.next = intbv(i)
                 yield delay(10)
@@ -41,8 +39,11 @@ class TestOriginalGrayCode(TestCase):
         for n in range(2, MAX_WIDTH):
             Ln = nextLn(Ln)
             del Rn[:]
+            B = Signal(intbv(-1))
+            G = Signal(intbv(0))
             dut = bin2gray(B, G, n)
-            sim = Simulation(dut, stimulus(n))
+            stim = stimulus(B, G, n)
+            sim = Simulation(dut, stim)
             sim.run(quiet=1)
             self.assertEqual(Ln, Rn)
 
@@ -52,12 +53,8 @@ class TestGrayCodeProperties(TestCase):
     def testSingleBitChange(self):
         
         """ Check that only one bit changes in successive codewords """
-
-        B = Signal(intbv(-1))
-        G = Signal(intbv(0))
-        G_Z = Signal(intbv(0))
         
-        def test(width):
+        def test(B, G, width):
             B.next = intbv(0)
             yield delay(10)
             for i in range(1, 2**width):
@@ -67,9 +64,13 @@ class TestGrayCodeProperties(TestCase):
                 diffcode = bin(G ^ G_Z)
                 self.assertEqual(diffcode.count('1'), 1)
         
-        for width in range(MAX_WIDTH):
+        for width in range(1, MAX_WIDTH):
+            B = Signal(intbv(-1))
+            G = Signal(intbv(0))
+            G_Z = Signal(intbv(0))
             dut = bin2gray(B, G, width)
-            sim = Simulation(dut, test(width))
+            check = test(B, G, width)
+            sim = Simulation(dut, check)
             sim.run(quiet=1)
 
 
@@ -77,10 +78,7 @@ class TestGrayCodeProperties(TestCase):
         
         """ Check that all codewords occur exactly once """
 
-        B = Signal(intbv(-1))
-        G = Signal(intbv(0))
-
-        def test(width):
+        def test(B, G, width):
             actual = []
             for i in range(2**width):
                 B.next = intbv(i)
@@ -90,9 +88,12 @@ class TestGrayCodeProperties(TestCase):
             expected = range(2**width)
             self.assertEqual(actual, expected)
        
-        for width in range(MAX_WIDTH):
+        for width in range(1, MAX_WIDTH):
+            B = Signal(intbv(-1))
+            G = Signal(intbv(0))
             dut = bin2gray(B, G, width)
-            sim = Simulation(dut, test(width))
+            check = test(B, G, width)
+            sim = Simulation(dut, check)
             sim.run(quiet=1)
             
 
