44468d3868111bfdca463437d07e0680e5543a7d
jand <none@none>
2003-06-02 14:34:18 +0000

    Put run back where it belongs

--- myhdl/Simulation.py
+++ myhdl/Simulation.py
@@ -37,8 +37,11 @@ from Cosimulation import Cosimulation
 from join import join
 from _Waiter import _Waiter
 from util import StopSimulation, SuspendSimulation
-import simrun
-import simrunc
+try:
+    import simrunc
+except:
+    pass
+
 
 schedule = _futureEvents.append
 
@@ -79,6 +82,7 @@ class Simulation(object):
         del _futureEvents[:]
         del _siglist[:]
         
+        
     def _finalize(self):
         cosim = self._cosim
         if cosim:
@@ -86,11 +90,120 @@ class Simulation(object):
             os.close(cosim._rt)
             os.close(cosim._wf)
             os.waitpid(cosim._child_pid, 0)
+            
         
     def runc(self, duration=0, quiet=0):
         simrunc.run(sim=self, duration=duration, quiet=quiet)
-         
-    run = simrun.run
+
+
+    def run(self, duration=None, quiet=0):
+
+        """ Run the simulation for some duration.
+
+        duration -- specified simulation duration (default: forever)
+        quiet -- don't print StopSimulation messages (default: off)
+
+        """
+
+        waiters = self._waiters
+        maxTime = None
+        if duration:
+            stop = _Waiter(None)
+            stop.hasRun = 1
+            maxTime = _simulator._time + duration
+            schedule((maxTime, stop))
+        cosim = self._cosim
+        t = _simulator._time
+        actives = {}
+
+        while 1:
+            try:
+
+                for s in _siglist:
+                    waiters.extend(s._update())
+                del _siglist[:]
+
+                while waiters:
+                    waiter = waiters.pop()
+                    if waiter.hasRun or not waiter.hasGreenLight():
+                        continue
+                    try:
+                        clauses, clone = waiter.next()
+                    except StopIteration:
+                        if waiter.caller:
+                            waiters.append(waiter.caller)
+                        continue
+                    nr = len(clauses)
+                    for clause in clauses:
+                        if type(clause) is _WaiterList:
+                            clause.append(clone)
+                            if nr > 1:
+                                actives[id(clause)] = clause
+                        elif isinstance(clause, Signal):
+                            wl = clause._eventWaiters
+                            wl.append(clone)
+                            if nr > 1:
+                                actives[id(wl)] = wl
+                        elif type(clause) is delay:
+                            schedule((t + clause._time, clone))
+                        elif type(clause) is GeneratorType:
+                            waiters.append(_Waiter(clause, clone))
+                        elif type(clause) is join:
+                            waiters.append(_Waiter(clause._generator(), clone))
+                        elif clause is None:
+                            waiters.append(clone)
+                        else:
+                            raise TypeError, "Incorrect yield clause type"
+
+                if cosim:
+                    cosim._get()
+                    if _siglist or cosim._hasChange:
+                        cosim._put(t)
+                        continue
+                elif _siglist:
+                    continue
+
+                if actives:
+                    for wl in actives.values():
+                        wl.purge()
+                    actives = {}
+
+                if _futureEvents:
+                    if t == maxTime:
+                        raise SuspendSimulation, \
+                              "Simulated for duration %s" % duration
+                    _futureEvents.sort()
+                    t = _simulator._time = _futureEvents[0][0]
+                    if cosim:
+                        cosim._put(t)
+                    while _futureEvents:
+                        newt, event = _futureEvents[0]
+                        if newt == t:
+                            if type(event) is _Waiter:
+                                waiters.append(event)
+                            else:
+                                waiters.extend(event.apply())
+                            del _futureEvents[0]
+                        else:
+                            break
+                else:
+                    raise StopSimulation, "No more events"
+
+            except SuspendSimulation:
+                if not quiet:
+                    printExcInfo()
+                return 1
+
+            except StopSimulation:
+                if not quiet:
+                    printExcInfo()
+                self._finalize()
+                return 0
+
+            except:
+                self._finalize()
+                raise
+        
 
 
 def printExcInfo():
