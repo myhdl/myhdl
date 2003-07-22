6d144f51a07d2e1437c2ece5bd5f1476de0421af
jand <none@none>
2003-07-22 09:14:18 +0000

    added enum test

--- myhdl/enum.py
+++ myhdl/enum.py
@@ -1,9 +1,36 @@
+#  This file is part of the myhdl library, a Python package for using
+#  Python as a Hardware Description Language.
+#
+#  Copyright (C) 2003 Jan Decaluwe
+#
+#  The myhdl library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation; either version 2.1 of the
+#  License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+""" Module that implements enum.
+
+"""
+
+__author__ = "Jan Decaluwe <jan@jandecaluwe.com>"
+__revision__ = "$Revision$"
+__date__ = "$Date$"
+
 from types import StringType
 
 
 def enum(*args):
 
-    args = list(args)
+    # args = args
     # only default encoding for now
     argdict = {}
     encoding = {}
@@ -11,8 +38,8 @@ def enum(*args):
     for arg in args:
         if type(arg) is not StringType:
             raise TypeError
-        if argdict.has_key(arg):
-            raise ValueError
+        if encoding.has_key(arg):
+            raise ValueError("enum literals should be unique")
         argdict[i] = arg
         encoding[arg] = i
         i += 1
@@ -29,14 +56,11 @@ def enum(*args):
             for slot in args:
                 self.__dict__[slot] = EnumItem(slot)
         def __setattr__(self, attr, val):
-            raise AttributeError
+            raise AttributeError("Cannot assign to enum attributes")
         def __len__(self):
             return len(args)
         def __repr__(self):
-            s = ""
-            for arg in args:
-                s += "%s=%d, " % (arg, encoding[arg])
-            return "<Enum: %s>" % s[:-2]
+            return "<Enum: %s>" % ", ".join(args)
         __str__ = __repr__
         
     return Enum()
---
+++ myhdl/test_enum.py
@@ -0,0 +1,85 @@
+#  This file is part of the myhdl library, a Python package for using
+#  Python as a Hardware Description Language.
+#
+#  Copyright (C) 2003 Jan Decaluwe
+#
+#  The myhdl library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation; either version 2.1 of the
+#  License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+""" Run the unit tests for enum """
+
+__author__ = "Jan Decaluwe <jan@jandecaluwe.com>"
+__revision__ = "$Revision$"
+__date__ = "$Date$"
+
+from __future__ import generators
+
+import random
+from random import randrange
+random.seed(1) # random, but deterministic
+
+import unittest
+from unittest import TestCase
+import sys
+
+from myhdl import enum
+
+
+class TestEnum(TestCase):
+
+    t_State = enum("HUNT", "CONFIRM", "SYNC")
+    t_Homograph = enum("HUNT", "CONFIRM", "SYNC")
+
+    def testUniqueLiterals(self):
+        try:
+            t_State = enum("HUNT", "CONFIRM", "HUNT")
+        except ValueError:
+            pass
+        else:
+            self.fail()
+
+    def testWrongAttr(self):
+        try:
+            self.t_State.TYPO
+        except AttributeError:
+            pass
+        else:
+            self.fail()
+
+    def testAttrAssign(self):
+        self.t_State.HUNT
+        try:
+            self.t_State.HUNT = 4
+        except AttributeError:
+            pass
+        else:
+            self.fail()
+
+    def testWrongAttrAssign(self):
+        try:
+            self.t_State.TYPO = 4
+        except AttributeError:
+            pass
+        else:
+            self.fail()
+
+    def testHomograph(self):
+        self.assert_(self.t_State is not self.t_Homograph)
+        
+    def testHomographLiteral(self):
+        self.assert_(self.t_State.HUNT is not self.t_Homograph.HUNT)
+
+
+if __name__ == "__main__":
+    unittest.main()
