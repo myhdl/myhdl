f0e6fa4647e1112eec89f2424145e80084a5eca5
jand <none@none>
2003-07-23 08:51:44 +0000

    min max public attributes

--- myhdl/test_Signal.py
+++ myhdl/test_Signal.py
@@ -58,9 +58,9 @@ class SigTest(TestCase):
         self.negedgeWaiters = [object() for i in range(7)]
 
     def testPublicInterface(self):
-        """ public interface of a sig: val, next, posedge, negedge"""
+        """ public interface of a sig: val, next, posedge, negedge, min, max"""
         s1 = Signal(1)
-        expected = ['next', 'val', 'posedge', 'negedge']
+        expected = ['next', 'val', 'posedge', 'negedge', 'min', 'max']
         iface = [attr for attr in dir(s1) if attr[0] != '_']
         expected.sort()
         iface.sort()
