7d68a65274f49af5c2992b5b2a6ae0227ee4fb56
jand <none@none>
2004-01-07 21:28:04 +0000

    0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import sys
 
-versionError = "ERROR: myhdl requires Python 2.2.2 or higher"
+versionError = "ERROR: myhdl requires Python 2.3 or higher"
 
 # use version_info to check version
 # this was new in 2.0, so first see if it exists
@@ -11,21 +11,34 @@ try:
 except:
     print versionError
     raise SystemExit(1)
-# we need at least 2.2.2
-if sys.version_info[:3] < (2, 2, 2):
+# we need at least 2.3
+if sys.version_info < (2, 3):
     print versionError
     raise SystemExit(1)
 
 from distutils.core import setup
+
+classifiers = """\
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)
+"""
+
     
 setup(name="myhdl",
-      version="0.3",
+      version="0.4",
       description="Python as a Hardware Description Language",
+      long_description = "See home page.",
       author="Jan Decaluwe",
       author_email="jan@jandecaluwe.com",
       url="http://jandecaluwe.com/Tools/MyHDL/Overview.html",
-      download_url="http://jandecaluwe.com/Tools/MyHDL/Download.html",
+      download_url="http://sourceforge.net/project/showfiles.php?group_id=91207",
       packages=['myhdl'],
       license="LGPL",
+      keywords="HDL ASIC FPGA hardware design",
+      classifiers=filter(None, classifiers.split("\n")),
       )
       
