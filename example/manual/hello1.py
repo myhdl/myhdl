0b53eb97ec87d4558808256259f9dceee3f62344
jand <none@none>
2005-12-05 12:22:35 +0000

    adapted to 0.5

--- example/manual/hello1.py
+++ example/manual/hello1.py
@@ -1,14 +1,18 @@
-from __future__ import generators
-from myhdl import Signal, delay, posedge, now, Simulation
+from myhdl import Signal, delay, always, now, Simulation
 
 def sayHello():
-    while 1:
-        yield delay(10)
+    
+    @always(delay(10))
+    def behavior():
         print "%s Hello World!" % now()
+
+    return behavior
+
 
 def main():
     sim = Simulation(sayHello())
     sim.run(30)
+    
 
 if __name__ == '__main__':
     main()
