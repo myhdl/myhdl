cb507cc8dbff182c39287563badb025ce437494a
jand <none@none>
2005-12-19 12:55:44 +0000

    fsm

--- myhdl/test/toVerilog/test_fsm.py
+++ myhdl/test/toVerilog/test_fsm.py
@@ -14,7 +14,7 @@ t_State_b = enum('SEARCH', 'CONFIRM', 'SYNC')
 t_State_oh = enum('SEARCH', 'CONFIRM', 'SYNC', encoding="one_hot")
 t_State_oc = enum('SEARCH', 'CONFIRM', 'SYNC', encoding="one_cold")
 
-def FramerCtrl_ref(SOF, state, syncFlag, clk, reset_n, t_State):
+def FramerCtrl(SOF, state, syncFlag, clk, reset_n, t_State):
     
     """ Framing control FSM.
 
@@ -28,36 +28,34 @@ def FramerCtrl_ref(SOF, state, syncFlag, clk, reset_n, t_State):
     
     index = Signal(intbv(0)[8:]) # position in frame
 
+    @always(clk.posedge, reset_n.negedge)
     def FSM():
-        while 1:
-            yield clk.posedge, reset_n.negedge
-            if reset_n == ACTIVE_LOW:
-                SOF.next = 0
-                index.next = 0
-                state.next = t_State.SEARCH
-            else:
-                index.next = (index + 1) % FRAME_SIZE
-                SOF.next = 0
-                if state == t_State.SEARCH:
-                    index.next = 1
+        if reset_n == ACTIVE_LOW:
+            SOF.next = 0
+            index.next = 0
+            state.next = t_State.SEARCH
+        else:
+            index.next = (index + 1) % FRAME_SIZE
+            SOF.next = 0
+            if state == t_State.SEARCH:
+                index.next = 1
+                if syncFlag:
+                    state.next = t_State.CONFIRM
+            elif state == t_State.CONFIRM:
+                if index == 0:
                     if syncFlag:
-                        state.next = t_State.CONFIRM
-                elif state == t_State.CONFIRM:
-                    if index == 0:
-                        if syncFlag:
-                            state.next = t_State.SYNC
-                        else:
-                            state.next = t_State.SEARCH
-                elif state == t_State.SYNC:
-                    if index == 0:
-                        if not syncFlag:
-                            state.next = t_State.SEARCH
-                    SOF.next = (index == FRAME_SIZE-1)
-                else:
-                    raise ValueError("Undefined state")
-
-    FSM_1 = FSM()
-    return FSM_1
+                        state.next = t_State.SYNC
+                    else:
+                        state.next = t_State.SEARCH
+            elif state == t_State.SYNC:
+                if index == 0:
+                    if not syncFlag:
+                        state.next = t_State.SEARCH
+                SOF.next = (index == FRAME_SIZE-1)
+            else:
+                raise ValueError("Undefined state")
+            
+    return FSM
 
 
 def FramerCtrl_alt(SOF, state, syncFlag, clk, reset_n, t_State):
@@ -110,7 +108,7 @@ def FramerCtrl_alt(SOF, state, syncFlag, clk, reset_n, t_State):
     return FSM_1
 
 
-def FramerCtrl(SOF, state, syncFlag, clk, reset_n, t_State):
+def FramerCtrl_ref(SOF, state, syncFlag, clk, reset_n, t_State):
     
     """ Framing control FSM.
 
