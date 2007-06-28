b632bc0bd15a03ffa7f7624e911eedae2f35c77a
jand <none@none>
2007-06-28 20:36:39 +0000

    first tests

---
+++ myhdl/test/conversion/test_bin2gray.py
@@ -0,0 +1,70 @@
+import os
+path = os.path
+
+from myhdl import *
+from myhdl.conversion import verify
+
+def bin2gray2(B, G, width):
+    """ Gray encoder.
+
+    B -- input intbv signal, binary encoded
+    G -- output intbv signal, gray encoded
+    width -- bit width
+    """
+    Bext = intbv(0)[width+1:]
+    while 1:
+        yield B
+        Bext[:] = B
+        for i in range(width):
+            G.next[i] = Bext[i+1] ^ Bext[i]
+
+def bin2gray(B, G, width):
+    
+    """ Gray encoder.
+
+    B -- input intbv signal, binary encoded
+    G -- output intbv signal, gray encoded
+    width -- bit width
+    
+    """
+
+    @always_comb
+    def logic():
+        Bext = intbv(0)[width+1:]
+        Bext[:] = B
+        for i in range(width):
+            G.next[i] = Bext[i+1] ^ Bext[i]
+
+    return logic
+           
+      
+ 
+def bin2grayBench(width, bin2gray):
+
+    B = Signal(intbv(0)[width:])
+    G = Signal(intbv(0)[width:])
+
+    bin2gray_inst = bin2gray(B, G, width)
+
+    n = 2**width
+
+    @instance
+    def stimulus():
+        for i in range(n):
+            B.next = i
+            yield delay(10)
+            #print "B: " + bin(B, width) + "| G_v: " + bin(G_v, width)
+            #print bin(G, width)
+            #print bin(G_v, width)
+            print "%d" % G
+
+
+    return stimulus, bin2gray_inst
+
+
+def test1():
+    assert verify(bin2grayBench, width=8, bin2gray=bin2gray) == 0
+    
+def test2():
+    assert verify(bin2grayBench, width=8, bin2gray=bin2gray2) == 0
+
