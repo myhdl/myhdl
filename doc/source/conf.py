fdfead0356480432134813c5c995e0cc0281b676
Jan Decaluwe <jan@jandecaluwe.com>
2008-11-07 16:18:33 +0100

    Custom stylesheet

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -74,7 +74,7 @@ pygments_style = 'sphinx'
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+html_style = 'myhdl.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
