6e3df2c43965a68c4981e51dae7c88ed01c163f0
Jan Decaluwe <jan@jandecaluwe.com>
2013-04-10 14:30:45 +0200

    Solved bug 28

---
+++ myhdl/test/bugs/test_bug_28.py
@@ -0,0 +1,16 @@
+from myhdl import *
+
+def bug_28(dout, channel):
+    @always_comb
+    def comb():
+        dout.next = 0x8030 + (channel << 10)
+    return comb
+
+dout = Signal(intbv(0)[16:0])
+channel = Signal(intbv(0)[4:0])
+
+def test_bug_28():
+    try:
+        toVHDL(bug_28, dout, channel)
+    except:
+        raise
