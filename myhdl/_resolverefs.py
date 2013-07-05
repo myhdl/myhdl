a5b197b6bd36afcdd623ae58429cc0e346a18e94
Keerthan Jaic <kjaic@clemson.edu>
2013-07-05 18:53:58 +0000

    Removed redundant line.
    --HG--
    branch : mep107

--- myhdl/_resolverefs.py
+++ myhdl/_resolverefs.py
@@ -53,5 +53,4 @@ class _AttrRefTransformer(ast.NodeTransformer):
         nodes = _flatten(node.body, node.args)
         for n in nodes:
             self.visit(n)
-        self.visit(node.args)
         return node
