99241ab49a0a76472bb6f107a078248782af9626
Keerthan Jaic <jckeerthan@gmail.com>
2015-02-01 18:51:18 -0500

    fix string_types in _compat

--- myhdl/_compat.py
+++ myhdl/_compat.py
@@ -4,12 +4,12 @@ PY2 = sys.version_info[0] == 2
 
 
 if not PY2:
-    string_types = (str, unicode)
+    string_types = (str,)
     integer_types = (int,)
     long = int
     import builtins
 else:
-    string_types = (str,)
+    string_types = (str, unicode)
     integer_types = (int, long)
     long = long
     import __builtin__ as builtins
