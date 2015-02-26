0945697fc85e8cfe685aa3537834c0cd68c19bb1
Henry Gomersall <heng@kedevelopments.co.uk>
2015-02-26 12:52:00 +0000

    Made sure the myhdl VHDL support package is also placed in the desired directory when the directory attribute of toVHDL is set.

--- myhdl/test/conversion/general/test_set_dir.py
+++ myhdl/test/conversion/general/test_set_dir.py
@@ -4,6 +4,10 @@ from myhdl import *
 from tempfile import mkdtemp
 from shutil import rmtree
 
+import myhdl
+_version = myhdl.__version__.replace('.','')
+_shortversion = _version.replace('dev','')
+
 def simple_dir_model(din, dout, clk):
     """ Simple convertible model """
 
@@ -36,6 +40,30 @@ def test_toVHDL_set_dir():
         toVHDL.directory = None        
         rmtree(tmp_dir)
 
+def test_toVHDL_myhdl_package_set_dir():
+    '''In order that the MyHDL package files are located in the 
+    same place as the generated VHDL files, when the directory attribute of 
+    toVHDL is set, this location should be used for the generated MyHDL 
+    package files.
+    '''
+    tmp_dir = mkdtemp()
+
+    din = Signal(intbv(0)[5:])
+    dout = Signal(intbv(0)[5:])
+    clock = Signal(bool(0))
+
+    try:
+        toVHDL.directory = tmp_dir
+        toVHDL(simple_dir_model, din, dout, clock)
+
+        assert os.path.exists(
+            os.path.join(tmp_dir, "pck_myhdl_%s.vhd" % _shortversion))
+
+    finally:
+
+        toVHDL.directory = None
+
+        rmtree(tmp_dir)
 
 def test_toVerilog_set_dir():
     '''In order that a developer can define where in the project 
@@ -62,6 +90,7 @@ def test_toVerilog_set_dir():
         toVerilog.directory = None
         toVerilog.no_testbench = no_testbench_state
         rmtree(tmp_dir)
+
 
 def test_toVerilog_testbench_set_dir():
     '''In order that generated Verilog test bench files are located in the 
