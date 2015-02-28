426dfd8246926e7dcaddc1bb8d93aa5078bcee4b
Shen Chen <shenchen@cogenda.com>
2015-03-01 03:03:09 +0800

    pretty print messages during test. exit with nonzero retcode if any test fails.

--- ci.sh
+++ ci.sh
@@ -1,19 +1,91 @@
 #!/bin/bash
 
+
+# {{{ Utilities
+# {{{ cfont
+cfont() {
+while (($#!=0))
+do
+  case $1 in
+    -b)           echo -ne " " ;;
+    -t)           echo -ne "\t";;
+    -n)           echo -ne "\n";;
+    -black)       echo -ne "\033[30m";;
+    -red)         echo -ne "\033[31m";;
+    -green)       echo -ne "\033[32m";;
+    -yellow)      echo -ne "\033[33m";;
+    -blue)        echo -ne "\033[34m";;
+    -purple)      echo -ne "\033[35m";;
+    -cyan)        echo -ne "\033[36m";;
+    -white|-gray) echo -ne "\033[37m";;
+    -reset)       echo -ne "\033[0m";;
+    -h|-help|--help)
+      echo "Usage: cfont -color1 message1 -color2 message2 ...";
+      echo "eg:       cfont -red [ -blue message1 message2 -red ]";
+      ;;
+    *)
+      echo -ne "$1"
+      ;;
+  esac
+  shift
+done
+}
+# }}}
+
+# {{{ print_heading
+print_heading() {
+  msg="$@"
+
+  line=$(printf '%0.1s' "="{1..80})
+
+  lPad=$(( ( 80 - ${#msg} ) / 2 - 1 ))
+  [[ $lPad -lt 0 ]] && lPad=0
+  pad="${line::$lPad}"
+
+  cfont -cyan
+  echo $line
+  echo $pad $msg $pad
+  echo $line
+  cfont -reset
+}
+# }}}
+
+# {{{ run_test
+run_test() {
+  cfont -cyan; echo "running test: $command $@" ; cfont -n -reset
+  $command "$@"
+
+  if [ $? -ne 0 ]; then
+    cfont -red; echo "test failed: $command $@" ; cfont -n -reset
+    foundError=1
+  else
+    cfont -green "OK" -n -reset
+  fi
+}
+# }}}
+
+# }}}
+
+foundError=0
 echo "Running $CI_TARGET tests"
 
+CI_TARGET=${CI_TARGET:-core}
 if [ "$CI_TARGET" == "core" ]; then
-  make -C myhdl/test/core
-  make -C myhdl/test/core2
-elif [ "$CI_TARGET" == "icarus" ]; then
-  echo "======================================================================="
-  echo "=========== A. Test Converted Verilog Code                    ========="
-  echo "======================================================================="
-  make -C "myhdl/test/conversion/general" icarus
 
-  echo "======================================================================="
-  echo "=========== B. Test Co-Simulation with Converted Verilog Code ========="
-  echo "======================================================================="
-  make -C cosimulation/icarus
-  make -C myhdl/test/conversion/toVerilog
+  print_heading "Core MyHDL Testbench"
+  run_test make -C myhdl/test/core
+  run_test make -C myhdl/test/core2
+
+elif [ "$CI_TARGET" == "icarus" ]; then
+
+  print_heading "Test Converted Verilog Code"
+  run_test make -C "myhdl/test/conversion/general" icarus
+
+  print_heading "Test Co-Simulation with Converted Verilog Code" 
+  run_test make -C cosimulation/icarus
+  run_test make -C myhdl/test/conversion/toVerilog
+
 fi
+
+exit $foundError
+
