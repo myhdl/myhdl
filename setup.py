f93ab22e0ed3c108c63c5b73c415ab926fc1b7b4
Keerthan Jaic <jckeerthan@gmail.com>
2015-03-29 10:46:15 -0400

    distribute cosim source code with package
    the source code will be put in sys.prefix/share/myhdl/cosimulation.
    sys.prefix will be /usr or /usr/local for global install, or root dir of
    virtualenv.
    
    This has two benifits:
    1. Distro packages/pip installs will come with the cosim source code, so
       users don't need to clone the git repo.
    2. Tools can easily automate compilation of the cosimulation vpis.
    
    fixes jck/uhdl#2

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 """ myhdl's distribution and installation script. """
 
 import ast
+import fnmatch
 import re
+import os
 import sys
+
+from collections import defaultdict
 
 if sys.version_info < (2, 6) or (3, 0) <= sys.version_info < (3, 4):
     raise RuntimeError("Python version 2.6, 2.7 or >= 3.4 required.")
@@ -21,6 +25,13 @@ with open('myhdl/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+data_root = 'share/myhdl'
+cosim_data = defaultdict(list)
+for base, dir, files in os.walk('cosimulation'):
+    for pat in ('*.c', 'Makefile*', '*.py', '*.v', '*.txt'):
+        good = fnmatch.filter(files, pat)
+        if good:
+            cosim_data[base].extend(os.path.join(base,f) for f in good)
 
 setup(
     name="myhdl",
@@ -32,6 +43,7 @@ setup(
     url="http://www.myhdl.org",
     download_url="https://bitbucket.org/jandecaluwe/myhdl/get/0.8.1.zip",
     packages=['myhdl', 'myhdl.conversion'],
+    data_files=[(os.path.join(data_root, k), v) for k, v in cosim_data.items()],
     license="LGPL",
     platforms='any',
     keywords="HDL ASIC FPGA hardware design",
