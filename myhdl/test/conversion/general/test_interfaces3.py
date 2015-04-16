08519b452f153885d3cc43038111b7779e2bb2f1
Keerthan Jaic <jckeerthan@gmail.com>
2015-04-15 23:07:12 -0400

    Rename attribute references in the AST before analysis
    fixes #33

--- myhdl/test/conversion/general/test_interfaces3.py
+++ myhdl/test/conversion/general/test_interfaces3.py
@@ -188,7 +188,6 @@ def test_three_analyze():
     intf = IntfWithConstant2()
     analyze(m_top_const, clock, reset, x, y, intf)
 
-@bug('33', 'vhdl')
 def test_three_verify():
     assert verify(c_testbench_three) == 0
 
