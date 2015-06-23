myhdl/myhdl

commit 08519b452f153885d3cc43038111b7779e2bb2f1

Rename attribute references in the AST before analysis
fixes #33

myhdl/_resolverefs.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import ast
+import itertools
 from types import FunctionType
 
 from myhdl._util import _flatten, _makeAST, _genfunc
@@ -24,6 +25,10 @@ def _resolveRefs(symdict, arg):
 
 #TODO: Refactor this into two separate nodetransformers, since _resolveRefs
 #needs only the names, not the objects
+
+def _suffixer(name):
+    suffixed_names = (name+'_renamed{0}'.format(i) for i in itertools.count())
+    return itertools.chain([name], suffixed_names)
 
 
 class _AttrRefTransformer(ast.NodeTransformer):
@@ -52,13 +57,12 @@ class _AttrRefTransformer(ast.NodeTransformer):
                 return node
 
         attrobj = getattr(obj, node.attr)
-        new_name = node.value.id+'.'+node.attr
-        if new_name not in self.data.symdict:
-            self.data.symdict[new_name] = attrobj
-            self.data.objlist.append(new_name)
-        else:
-            pass
-            #assert self.data.symdict[new_name] == attrobj
+
+        name = node.value.id + '_' + node.attr
+        new_name = next(s for s in _suffixer(name) if s not in self.data.symdict)
+        self.data.symdict[new_name] = attrobj
+        self.data.objlist.append(new_name)
+
         new_node = ast.Name(id=new_name, ctx=node.value.ctx)
         return ast.copy_location(new_node, node)
 

myhdl/conversion/_analyze.py
@@ -57,15 +57,6 @@ _extConstDict = {}
 
 
 def _makeName(n, prefixes, namedict):
-    #Take care of names with periods
-    #For attribute references, periods are replaced with '_'.
-    if '.' in n:
-        n = n.replace('.', '_')
-        if n in namedict:
-            i = 0
-            while (n + '_{0}'.format(i)) in namedict:
-                i += 1
-            n += '_{0}'.format(i)
     # trim empty prefixes
     prefixes = [p for p in prefixes if p]
     if len(prefixes) > 1:

myhdl/test/conversion/general/test_interfaces3.py
@@ -188,7 +188,6 @@ def test_three_analyze():
     intf = IntfWithConstant2()
     analyze(m_top_const, clock, reset, x, y, intf)
 
-@bug('33', 'vhdl')
 def test_three_verify():
     assert verify(c_testbench_three) == 0
 

commit 7fea7bdd9dccb23d8aa235215b8aefa2b016541f

avoid renaming attribute references twice
This commit fixes bugs introduced by 08519b4
Fixes #82

myhdl/_resolverefs.py
@@ -26,9 +26,10 @@ def _resolveRefs(symdict, arg):
 #TODO: Refactor this into two separate nodetransformers, since _resolveRefs
 #needs only the names, not the objects
 
-def _suffixer(name):
+def _suffixer(name, used_names):
     suffixed_names = (name+'_renamed{0}'.format(i) for i in itertools.count())
-    return itertools.chain([name], suffixed_names)
+    new_names = itertools.chain([name], suffixed_names)
+    return next(s for s in new_names if s not in used_names)
 
 
 class _AttrRefTransformer(ast.NodeTransformer):
@@ -36,6 +37,7 @@ class _AttrRefTransformer(ast.NodeTransformer):
         self.data = data
         self.data.objlist = []
         self.myhdl_types = (EnumType, SignalType)
+        self.name_map = {}
 
     def visit_Attribute(self, node):
         self.generic_visit(node)
@@ -58,8 +60,11 @@ class _AttrRefTransformer(ast.NodeTransformer):
 
         attrobj = getattr(obj, node.attr)
 
-        name = node.value.id + '_' + node.attr
-        new_name = next(s for s in _suffixer(name) if s not in self.data.symdict)
+        orig_name = node.value.id + '.' + node.attr
+        if orig_name not in self.name_map:
+            base_name = node.value.id + '_' + node.attr
+            self.name_map[orig_name] = _suffixer(base_name, self.data.symdict)
+        new_name = self.name_map[orig_name]
         self.data.symdict[new_name] = attrobj
         self.data.objlist.append(new_name)
 

myhdl/test/conversion/general/test_interfaces4.py
@@ -141,7 +141,6 @@ def test_one_testbench():
     Simulation(c_testbench_one()).run()
 
 
-@bug('82')
 def test_one_analyze():
     clock = Signal(bool(0))
     reset = ResetSignal(0, active=1, async=False)
@@ -150,11 +149,9 @@ def test_one_analyze():
     analyze(m_top, clock, reset, sdi, sdo)
 
 
-@bug('82')
 def test_one_verify():
     assert verify(c_testbench_one) == 0
 
-@bug('82')
 def test_conversion():
     toVerilog(c_testbench_one)
     toVHDL(c_testbench_one)