9725ec59cafa1a287933cdff3f10096f3c43db2b
Keerthan Jaic <jckeerthan@gmail.com>
2015-07-18 09:14:44 -0400

    move set_inheritable to _compat

--- myhdl/_compat.py
+++ myhdl/_compat.py
@@ -13,6 +13,7 @@ if not PY2:
     class_types = (type,)
 
     from io import StringIO
+    from os import set_inheritable
     import builtins
 
     def to_bytes(s):
@@ -20,6 +21,7 @@ if not PY2:
 
     def to_str(b):
         return b.decode()
+
 else:
     string_types = (str, unicode)
     integer_types = (int, long)
@@ -31,3 +33,30 @@ else:
 
     to_bytes = _identity
     to_str = _identity
+
+    def set_inheritable(fd, inheritable):
+        # This implementation of set_inheritable is based on a code sample in
+        # [PEP 0446](https://www.python.org/dev/peps/pep-0446/) and on the
+        # CPython implementation of that proposal which can be browsed [here]
+        # (hg.python.org/releasing/3.4/file/8671f89107c8/Modules/posixmodule.c#l11130)
+        if sys.platform == "win32":
+            import msvcrt
+            import ctypes.windll.kernel32 as kernel32
+
+            HANDLE_FLAG_INHERIT = 1
+
+            if kernel32.SetHandleInformation(msvcrt.get_osfhandle(fd),
+                                             HANDLE_FLAG_INHERIT,
+                                             1 if inheritable else 0) == 0:
+                raise IOError("Failed on HANDLE_FLAG_INHERIT")
+        else:
+            import fcntl
+
+            fd_flags = fcntl.fcntl(fd, fcntl.F_GETFD)
+
+            if inheritable:
+                fd_flags &= ~fcntl.FD_CLOEXEC
+            else:
+                fd_flags |= fcntl.FD_CLOEXEC
+
+            fcntl.fcntl(fd, fcntl.F_SETFD, fd_flags)
