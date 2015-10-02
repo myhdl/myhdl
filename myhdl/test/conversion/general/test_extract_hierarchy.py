af84791ec8173c68a32c92cf273fe105c04c1c3c
Chris Felton <chris.felton@gmail.com>
2015-10-01 22:19:29 -0500

    moved hierarchy tests to test/conversion/general

--- myhdl/test/conversion/general/test_extract_hierarchy.py
+++ myhdl/test/conversion/general/test_extract_hierarchy.py
@@ -2,7 +2,8 @@
 import pytest
 
 import myhdl
-from myhdl import Signal, ResetSignal, always_seq, always_comb
+from myhdl import (Signal, ResetSignal, always_seq, always_comb,
+                   always)
 from myhdl.conversion import analyze
 
 """
@@ -24,10 +25,6 @@ function it is returned from.  The list.extend will "flatten" the
 generator objects from the return function to the calling 
 function.
 
-Use py.test to run the test
-
-  >> py.test sim=iverilog test_issue_123_1.py
-
 """
 
 port_map = {
@@ -38,6 +35,20 @@ port_map = {
      'y': Signal(bool(0)),
      'z': Signal(bool(0)),
 }
+
+
+me = 0
+port_map2 = {
+     'clock': Signal(bool(0)),
+     'x': Signal(bool(0)),
+     'y': Signal(bool(0))
+}
+
+
+class Interface:
+    def __init__(self):
+        self.x = Signal(bool(0))
+        self.y = Signal(bool(0))
 
 
 def mod_tuple(clock, reset, w, x, y, z):
@@ -61,6 +72,26 @@ def mod_list_1(clock, reset, w, x, y, z):
           z.next = b or y
      return [gen1, gen2]
 
+     
+def mod_with_interface_1(intf):
+    global me
+    xi = me
+    @always(intf.clock.posedge)
+    def rtl():
+        intf.y.next = (intf.x + xi) & 1
+    me += 1
+    return rtl
+
+
+def mod_with_interface_2(intf):
+    global me
+    xi = me
+    @always(intf.clock.posedge)
+    def rtl():
+        intf.y.next = (intf.x + xi) & 1
+    me += 1
+    return [rtl]
+     
 
 def hier_inconsistent_top_1(clock, reset, w, x, y, z):
      """ This is the expected failing version """
@@ -104,29 +135,73 @@ def hier_inconsistent_top_2(clock, reset, w, x, y, z):
      mod_insts += [gen1, gen2]
 
      return mod_insts
-     
 
-def test_issue_123_1():
-     myhdl.toVHDL.name = myhdl.toVerilog.name = 'issue_123_1_1'
+
+def list_of_interfaces_top_1(clock, x, y, number_of_modules=7):
+    loi = [Interface() for _ in range(number_of_modules)]
+    mods = []
+    for ii, intf in enumerate(loi):
+        if ii == 0:
+            intf.x = x
+        elif ii == number_of_modules-1:
+            intf.y = y
+        else:
+            intf.x = loi[ii-1].x
+        intf.clock = clock
+        mods += [mod_with_interface_1(intf)]
+    return mods
+
+
+def list_of_interfaces_top_2(clock, x, y, number_of_modules=7):
+    loi = [Interface() for _ in range(number_of_modules)]
+    mods = []
+    for ii, intf in enumerate(loi):
+        if ii == 0:
+            intf.x = x
+        elif ii == number_of_modules-1:
+            intf.y = y
+        else:
+            intf.x = loi[ii-1].x
+        intf.clock = clock
+        mods += mod_with_interface_2(intf)
+    return mods
+
+
+def test_extract_1():
+     myhdl.toVHDL.name = myhdl.toVerilog.name = 'extract_hier_1'
      myhdl.dump_hierarchy(mod_tuple, **port_map)
      assert analyze(mod_tuple, **port_map) == 0
 
 
-def test_issue_123_2():
-     myhdl.toVHDL.name = myhdl.toVerilog.name = 'issue_123_1_2'
+def test_extract_2():
+     myhdl.toVHDL.name = myhdl.toVerilog.name = 'extract_hier_2'
      myhdl.dump_hierarchy(mod_list_1, **port_map)
      assert analyze(mod_list_1, **port_map) == 0
 
 
-def test_issue_123_3():
-     myhdl.toVHDL.name = myhdl.toVerilog.name = 'issue_123_1_3'
+def test_extract_3():
+     myhdl.toVHDL.name = myhdl.toVerilog.name = 'extract_hier_3'
      myhdl.dump_hierarchy(hier_inconsistent_top_1, **port_map)
      assert analyze(hier_inconsistent_top_1, **port_map) == 0
 
 
-def test_issue_123_4():
-     myhdl.toVHDL.name = myhdl.toVerilog.name = 'issue_123_1_4'
+def test_extract_4():
+     myhdl.toVHDL.name = myhdl.toVerilog.name = 'extract_hier_4'
      myhdl.dump_hierarchy(hier_inconsistent_top_2, **port_map)
      with pytest.raises(myhdl.ExtractHierarchyError):
-          assert analyze(hier_inconsistent_top_2, **port_map) == 0
+         assert analyze(hier_inconsistent_top_2, **port_map) == 0
+
+         
+def test_extract_5():
+     myhdl.toVHDL.name = myhdl.toVerilog.name = 'extract_hier_5'
+     myhdl.dump_hierarchy(list_of_interfaces_top_1, **port_map2)
+     assert analyze(list_of_interfaces_top_1, **port_map2) == 0
+
+
+def test_extract_6():
+     myhdl.toVHDL.name = myhdl.toVerilog.name = 'extract_hier_6'
+     myhdl.dump_hierarchy(list_of_interfaces_top_2, **port_map2)
+     with pytest.raises(myhdl.ExtractHierarchyError):
+         assert analyze(list_of_interfaces_top_2, **port_map2) == 0
+         
 
