b81450ead2d36586cb67d645d559b907bd4ac5d9
Jan Decaluwe <jan@jandecaluwe.com>
2016-03-01 18:02:21 +0100

    typo

--- scripts/install_ghdl.sh
+++ scripts/install_ghdl.sh
@@ -1,4 +1,4 @@
-#!/bin/bash#
+#!/bin/bash
 set -evx
 wget https://sourceforge.net/projects/ghdl-updates/files/Builds/ghdl-0.33/debian/ghdl_0.33-1ubuntu1_amd64.deb -O /tmp/ghdl.tar.gz
 mkdir ghdl-0.33
