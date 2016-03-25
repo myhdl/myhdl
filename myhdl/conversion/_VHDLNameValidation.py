56429acf398fb42bc25f07b66315a86f8fa35240
Patrick Egan <PatrickEgan1996@gmail.com>
2016-03-25 15:59:21 -0400

    Encapsulated all of the name validation script into a class

--- myhdl/conversion/_VHDLNameValidation.py
+++ myhdl/conversion/_VHDLNameValidation.py
@@ -6,7 +6,9 @@ from myhdl import ToVHDLWarning
 
 #A list of all reserved words within VHDL which should not be used for
 #anything other than their own specific purpose
-_vhdl_keywords = ["abs", "access", "after", "alias", "all",
+
+class NameValidation:
+    _vhdl_keywords = ["abs", "access", "after", "alias", "all",
                   "and", "architecture", "array", "assert",
                   "attribute", "begin", "block", "body", "buffer",
                   "bus", "case", "component", "configuration",
@@ -26,22 +28,22 @@ _vhdl_keywords = ["abs", "access", "after", "alias", "all",
                   "unaffected", "units", "until", "use", "variable",
                   "wait", "when", "while", "with", "xnor", "xor"];
 
-#A list to hold all signal names being used in lowercase to raise an error
-#if no names are reused with different casing
-_usedNames = [];
+    #A list to hold all signal names being used in lowercase to raise an error
+    #if no names are reused with different casing
+    _usedNames = [];
 
-#Function which compares current parsed signal/entity to all keywords to
-#ensure reserved words are not being used for the wrong purpose
-def _nameValid(keyword):
-    for i in _vhdl_keywords:
-        if keyword == _vhdl_keywords[i]:
-            warnings.warn("VHDL keyword used: %s" % keyword, category=ToVHDLWarning)
-    for i in _usedNames:
-        if keyword.lower() == _usedNames[i]:
-            warnings.warn("Previously used name being reused: %s" % keyword, category=ToVHDLWarning)
-    _usedNames.append(keyword).lower
-    if keyword[0] == '_':
-        warnings.warn("VHDL variable names cannot contain '_': %s" % keyword, category=ToVHDLWarning)
-    for i in keyword:
-        if keyword[i] == '-':
-            warnings.warn("VHDL variable names cannot contain '-': %s" % keyword, category=ToVHDLWarning)
+    #Function which compares current parsed signal/entity to all keywords to
+    #ensure reserved words are not being used for the wrong purpose
+    def _nameValid(keyword):
+        for i in NameValidation._vhdl_keywords:
+            if keyword == NameValidation._vhdl_keywords[i]:
+                warnings.warn("VHDL keyword used: %s" % keyword, category=ToVHDLWarning)
+        for i in NameValidation._usedNames:
+            if keyword.lower() == NameValidation._usedNames[i]:
+                warnings.warn("Previously used name being reused: %s" % keyword, category=ToVHDLWarning)
+        NameValidation._usedNames.append(keyword).lower
+        if keyword[0] == '_':
+            warnings.warn("VHDL variable names cannot contain '_': %s" % keyword, category=ToVHDLWarning)
+        for i in keyword:
+            if keyword[i] == '-':
+                warnings.warn("VHDL variable names cannot contain '-': %s" % keyword, category=ToVHDLWarning)
