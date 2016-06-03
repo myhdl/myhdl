e2f559c883b20960b8fb4ee5f329c07380bd1e91
Christopher Felton <chris.felton@gmail.com>
2016-06-03 11:05:05 -0500

    use the Block methods and code cleanup

--- myhdl/test/conversion/general/test_interfaces1.py
+++ myhdl/test/conversion/general/test_interfaces1.py
@@ -1,48 +1,47 @@
 from __future__ import absolute_import
 
-import sys
+from myhdl import (block, Signal, ResetSignal, intbv, always_seq,
+                   instance, delay, StopSimulation, )
 
-import myhdl
-from myhdl import *
-from myhdl import ConversionError
-from myhdl.conversion._misc import _error
-from myhdl.conversion import analyze, verify
 
 class MyIntf(object):
     def __init__(self):
-        self.x = Signal(intbv(2,min=0,max=16))
-        self.y = Signal(intbv(3,min=0,max=18))
+        self.x = Signal(intbv(2, min=0, max=16))
+        self.y = Signal(intbv(3, min=0, max=18))
+
 
 @block
-def m_one_level(clock,reset,ia,ib):
+def one_level(clock, reset, ia, ib):
 
-    @always_seq(clock.posedge,reset=reset)
+    @always_seq(clock.posedge, reset=reset)
     def rtl():
         ia.x.next = ib.x + 1
         ia.y.next = ib.y + 1
 
     return rtl
 
-@block
-def m_two_level(clock,reset,ia,ib):
 
-    ic,ie = (MyIntf(),MyIntf(),)
-    g_one = m_one_level(clock,reset,ic,ie)
-    @always_seq(clock.posedge,reset=reset)
+@block
+def two_level(clock, reset, ia, ib):
+
+    ic, ie = MyIntf(), MyIntf()
+    one_inst = one_level(clock, reset, ic, ie)
+
+    @always_seq(clock.posedge, reset=reset)
     def rtl():
         ia.x.next = ib.x + ic.x
         ia.y.next = ib.y + ic.y
 
-    return g_one, rtl
+    return one_inst, rtl
+
 
 @block
 def c_testbench_one():
     clock = Signal(bool(0))
-    reset = ResetSignal(0,active=0,async=True)
-    ia = MyIntf()
-    ib = MyIntf()
+    reset = ResetSignal(0, active=0, async=True)
+    ia, ib = MyIntf(), MyIntf()
 
-    tb_dut = m_one_level(clock,reset,ia,ib)
+    tb_dut = one_level(clock, reset, ia, ib)
 
     @instance
     def tb_clk():
@@ -62,19 +61,19 @@ def c_testbench_one():
             yield clock.posedge
         assert ia.x == 3
         assert ia.y == 4
-        print("%d %d %d %d"%(ia.x,ia.y,ib.x,ib.y))
+        print("%d %d %d %d" % (ia.x, ia.y, ib.x, ib.y))
         raise StopSimulation
 
     return tb_dut, tb_clk, tb_stim
 
+
 @block
 def c_testbench_two():
     clock = Signal(bool(0))
-    reset = ResetSignal(0,active=0,async=True)
-    ia = MyIntf()
-    ib = MyIntf()
+    reset = ResetSignal(0, active=0, async=True)
+    ia, ib = MyIntf(), MyIntf()
 
-    tb_dut = m_two_level(clock,reset,ia,ib)
+    tb_dut = two_level(clock, reset, ia, ib)
 
     @instance
     def tb_clk():
@@ -94,36 +93,33 @@ def c_testbench_two():
             yield clock.posedge
         assert ia.x == 5
         assert ia.y == 7
-        print("%d %d %d %d"%(ia.x,ia.y,ib.x,ib.y))
+        print("%d %d %d %d" % (ia.x, ia.y, ib.x, ib.y))
         raise StopSimulation
 
     return tb_dut, tb_clk, tb_stim
 
+
 def test_one_level_analyze():
     clock = Signal(bool(0))
-    reset = ResetSignal(0,active=0,async=True)
-    ia = MyIntf()
-    ib = MyIntf()
-    analyze(m_one_level(clock,reset,ia,ib))
+    reset = ResetSignal(0, active=0, async=True)
+    ia, ib = MyIntf(), MyIntf()
+    inst = one_level(clock, reset, ia, ib)
+    assert inst.analyze_convert() == 0
+
 
 def test_one_level_verify():
-    assert verify(c_testbench_one()) == 0
+    inst = c_testbench_one()
+    assert inst.verify_convert() == 0
+
 
 def test_two_level_analyze():
     clock = Signal(bool(0))
-    reset = ResetSignal(0,active=0,async=True)
-    ia = MyIntf()
-    ib = MyIntf()
-    analyze(m_two_level(clock,reset,ia,ib))
+    reset = ResetSignal(0, active=0, async=True)
+    ia, ib = MyIntf(), MyIntf()
+    inst = two_level(clock, reset, ia, ib)
+    assert inst.analyze_convert() == 0
+
 
 def test_two_level_verify():
-    assert verify(c_testbench_two()) == 0
-
-
-if __name__ == '__main__':
-    print(sys.argv[1])
-    verify.simulator = analyze.simulator = sys.argv[1]
-    Simulation(c_testbench_one()).run()
-    Simulation(c_testbench_two()).run()
-    print(verify(c_testbench_one))
-    print(verify(c_testbench_two))
+    inst = c_testbench_two()
+    assert inst.verify_convert() == 0
