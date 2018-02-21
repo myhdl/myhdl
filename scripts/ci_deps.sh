ab8c802aa16c9f143bae094d54785000aa8e3b9e
Jan Decaluwe <jan@jandecaluwe.com>
2018-02-21 22:07:51 +0100

    try another fix

--- scripts/ci_deps.sh
+++ scripts/ci_deps.sh
@@ -2,7 +2,7 @@ if [ "$CI_TARGET" == "iverilog" ]; then
   sudo apt-get -qq update
   sudo apt-get install iverilog
 elif [ "$CI_TARGET" == "ghdl" ]; then
-  url=$(curl -s https://api.github.com/repos/ghdl/ghdl/releases/latest | jq -r ".assets[] | select (.name | test (\"ubuntu14\"))| .browser_download_url")
+  url=$(curl -s https://api.github.com/repos/ghdl/ghdl/releases/tag/v0.33 | jq -r ".assets[] | select (.name | test (\"ubuntu1_amd64\"))| .browser_download_url")
   curl -Lo ghdl.deb $url
   sudo dpkg -i ghdl.deb
   sudo apt-get install -f
