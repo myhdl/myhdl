096bad03a51393b44d2a10bea7b012c84daaf76c
udara28 <udara28@live.com>
2018-02-22 11:24:08 -0500

    Adding two tests for verifying shadow signal functionality after merging with latest master

--- myhdl/test/core/test_ShadowSignal.py
+++ myhdl/test/core/test_ShadowSignal.py
@@ -30,6 +30,28 @@ def bench_SliceSignal():
 def test_SliceSignal():
     Simulation(bench_SliceSignal()).run()
 
+def bench_SliceSlicedSignal():
+
+    s = Signal(intbv(0)[8:])
+    a, b = s(8,4), s(4,0)
+    aa, ab = a(4,2), a(2,0)
+    ba, bb = b(4,2), b(2,0)
+
+    @instance
+    def check():
+        for i in range(2**len(s)):
+            s.next = i
+            yield delay(10)
+            assert s[8:6] == aa
+            assert s[6:4] == ab
+            assert s[4:2] == ba
+            assert s[2:0] == bb
+
+    return check
+
+
+def test_SliceSlicedSignal():
+    Simulation(bench_SliceSlicedSignal()).run()
 
 def bench_ConcatSignal():
 
@@ -144,6 +166,39 @@ def bench_ConcatSignalWithNegs():
 def test_ConcatSignalWithNegs():
     Simulation(bench_ConcatSignalWithNegs()).run()
 
+
+def bench_ConcatConcatedSignal():
+
+    aa = Signal(intbv(0)[2:0])
+    ab = Signal(intbv(0)[2:0])
+    a = ConcatSignal(aa,ab)
+
+    ba = Signal(intbv(0)[2:0])
+    bb = Signal(intbv(0)[2:0])
+    b = ConcatSignal(ba,bb)
+
+    s = ConcatSignal(a,b)
+
+    @instance
+    def check():
+        for iaa in range(2**len(aa)):
+            for iab in range(2**len(ab)):
+                for iba in range(2**len(ba)):
+                    for ibb in range(2**len(bb)):
+                        aa.next = iaa
+                        ab.next = iab
+                        ba.next = iba
+                        bb.next = ibb
+                        yield delay(10)
+                        assert s[8:6] == aa
+                        assert s[6:4] == ab
+                        assert s[4:2] == ba
+                        assert s[2:0] == bb
+    return check
+
+
+def test_ConcatConcatedSignal():
+    Simulation(bench_ConcatConcatedSignal()).run()
 
 def bench_TristateSignal():
     s = TristateSignal(intbv(0)[8:])
