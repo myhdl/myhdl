6c25cb252a9ea5bef36ea82041d26d0285759e80
richmorj <54454625+richmorj@users.noreply.github.com>
2019-09-15 02:15:56 +0100

    Updating uart_tx example to use @block decorator and new API. (#320)
    * Updating uart_tx example to use @block decorator and new API.
    
    * Removed wildcard import of myhdl.
    
    * Removed unnecessary import myhdl.

--- example/uart_tx/uart_tx.py
+++ example/uart_tx/uart_tx.py
@@ -1,6 +1,6 @@
-import myhdl
-from myhdl import *
+from myhdl import always, always_seq, block, delay, enum, instance, intbv, ResetSignal, Signal, StopSimulation
 
+@block
 def uart_tx(tx_bit, tx_valid, tx_byte, tx_clk, tx_rst):
     
     index = Signal(intbv(0, min=0, max=8))
@@ -31,6 +31,7 @@ def uart_tx(tx_bit, tx_valid, tx_byte, tx_clk, tx_rst):
                     
     return fsm
 
+@block
 def uart_tx_2(tx_bit, tx_valid, tx_byte, tx_clk, tx_rst):
     
     index = Signal(intbv(0, min=0, max=8))
@@ -57,7 +58,7 @@ def uart_tx_2(tx_bit, tx_valid, tx_byte, tx_clk, tx_rst):
     return fsm
 
 
-
+@block
 def tb(uart_tx):   
       
     tx_bit = Signal(bool(1))
@@ -95,9 +96,11 @@ def tb(uart_tx):
     return clk_gen, stimulus, uart_tx_inst
 
 
-sim = Simulation(traceSignals(tb, uart_tx_2))
+dut = uart_tx_2
+inst = tb(dut)
 
-sim.run()
+inst.config_sim(trace=True)
+inst.run_sim(10000)
         
     
     
