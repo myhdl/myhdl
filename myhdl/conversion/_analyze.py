a25257ecdc7fcd8193a83c6daef3255be211ee4f
Josy Boelen <josyboelen@gmail.com>
2020-04-12 17:36:18 +0200

    Adding a commmnet to force commit

--- myhdl/conversion/_analyze.py
+++ myhdl/conversion/_analyze.py
@@ -1233,6 +1233,7 @@ def isboundmethod(m):
 def expandinterface(v, name, obj):
     for attr, attrobj in vars(obj).items():
         if isinstance(attrobj, _Signal):
+# override any 'mangled' name
 #             signame = attrobj._name
 #             if not signame:
             signame = name + '_' + attr
