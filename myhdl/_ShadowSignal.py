273812a5fc5936df1e71c56f1cf96e65a6a8c912
Josy Boelen <josy@citium.be>
2022-08-01 18:47:40 +0200

    Changed a docstring to launch GitHub actions on origin

--- myhdl/_ShadowSignal.py
+++ myhdl/_ShadowSignal.py
@@ -17,10 +17,10 @@
 #  License along with this library; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-""" Module that provides the ShadowSignal classes
-
-
+""" 
+    Module that provides the ShadowSignal classes
 """
+
 import warnings
 from copy import deepcopy
 
