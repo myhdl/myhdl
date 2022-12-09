f2aeedd9357ca9e1c2f5460c8da7f35a7be549ea
Dave Keeshan <dave.keeshan@daxzio.com>
2022-12-09 12:29:55 +0000

    Need to relook at this test, it performs differently for verilog and vhdl

--- myhdl/test/bugs/test_issue_98.py
+++ myhdl/test/bugs/test_issue_98.py
@@ -15,7 +15,7 @@ def issue_98(sda, scl, sda_i, sda_o, scl_i, scl_o):
          scl_d.next = None if not scl_o else 1 
      return hdl
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
 def test_issue_98_1():
     sda_i, sda_o, scl_i, scl_o = [Signal(False) for i in range(4)]
     sda, scl = [TristateSignal(False) for i in range(2)]
@@ -27,12 +27,12 @@ def test_issue_98_2():
     sda_i, sda_o, scl_i, scl_o = [Signal(intbv(0)[2:0]) for i in range(4)]
     sda, scl = [TristateSignal(intbv(0)[2:0]) for i in range(2)]
     toVHDL.name = toVerilog.name = 'issue_98_2'
-    assert issue_98(sda, scl, sda_i, sda_o, scl_i, scl_o).analyze_convert() == 1
+    assert issue_98(sda, scl, sda_i, sda_o, scl_i, scl_o).analyze_convert() == 0
 
 #@pytest.mark.xfail
 def test_issue_98_3():
     sda_i, sda_o, scl_i, scl_o = [Signal(intbv(0)[1:0]) for i in range(4)]
     sda, scl = [TristateSignal(intbv(0)[1:0]) for i in range(2)]
     toVHDL.name = toVerilog.name = 'issue_98_3'
-    assert issue_98(sda, scl, sda_i, sda_o, scl_i, scl_o).analyze_convert() == 1
+    assert issue_98(sda, scl, sda_i, sda_o, scl_i, scl_o).analyze_convert() == 0
 
