37f18d4d97e5b8deed50869c0fab26ce7db9075a
Dave Keeshan <dave.keeshan@daxzio.com>
2022-12-09 10:36:09 +0000

    Add explict test to check for deprecation case

---
+++ myhdl/test/conversion/general/test_convert_deprecation.py
@@ -0,0 +1,48 @@
+import myhdl
+from myhdl import *
+from myhdl import ConversionError
+from myhdl.conversion._misc import _error
+
+def bin2gray(B, G, width):
+
+    """ Gray encoder.
+
+    B -- input intbv signal, binary encoded
+    G -- output intbv signal, gray encoded
+    width -- bit width
+
+    """
+
+    @always_comb
+    def logic():
+        Bext = intbv(0)[width+1:]
+        Bext[:] = B
+        for i in range(width):
+            G.next[i] = Bext[i+1] ^ Bext[i]
+
+    return logic
+    
+
+def bin2grayBench(width, bin2gray):
+
+    B = Signal(intbv(0)[width:])
+    G = Signal(intbv(0)[width:])
+
+    bin2gray_inst = bin2gray(B, G, width)
+
+    n = 2**width
+
+    @instance
+    def stimulus():
+        for i in range(n):
+            B.next = i
+            yield delay(10)
+            print("%d" % G)
+
+    return stimulus, bin2gray_inst
+
+def testOldVerify():
+    conversion.verify(bin2grayBench, width=8, bin2gray=bin2gray)
+
+def testOldAnalyze():
+    conversion.analyze(bin2grayBench, width=8, bin2gray=bin2gray)
