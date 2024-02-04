89365fdf4d4b0f5fa2cebcd2caf472a43e8854a6
Josy <josy@citium.be>
2024-02-04 12:10:10 +0100

    correct initial_values for List Of Signal(enum)

--- myhdl/conversion/_toVHDL.py
+++ myhdl/conversion/_toVHDL.py
@@ -542,14 +542,20 @@ def _writeSigDecls(f, intf, siglist, memlist):
                     val_str = (
                         ' := (others => \'%s\')' % str(int(m.mem[0]._init)))
 
+                elif isinstance(m.mem[0]._init, EnumItemType):
+                    val_str = (' := (others => {})'.format(m.mem[0]._init._toVHDL()))
                 else:
                     val_str = (
                         ' := (others => %dX"%s")' %
                         (sig_vhdl_objs[0].size, str(m.mem[0]._init)))
             else:
-                _val_str = ',\n    '.join(
-                    ['%dX"%s"' % (obj.size, str(each._init)) for
-                     obj, each in zip(sig_vhdl_objs, m.mem)])
+                if isinstance(m.mem[0]._init, EnumItemType):
+                    _val_str = ',\n    '.join(
+                    '{}'.format(m.mem[0]._init._toVHDL()))
+                else:
+                    _val_str = ',\n    '.join(
+                        ['%dX"%s"' % (obj.size, str(each._init)) for
+                         obj, each in zip(sig_vhdl_objs, m.mem)])
 
                 val_str = ' := (\n    ' + _val_str + ')'
 
